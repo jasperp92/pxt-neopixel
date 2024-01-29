6706ac5766d96db9fadc909920463c3d68ddf7ea
Jasper Precht <72135430+jasperp92@users.noreply.github.com>
2024-01-29 11:46:10 +0100

    option to arrange neopixel matrix as snake pattern

--- neopixel.ts
+++ neopixel.ts
@@ -53,6 +53,7 @@ namespace neopixel {
         _length: number; // number of LEDs
         _mode: NeoPixelMode;
         _matrixWidth: number; // number of leds in a matrix - if any
+        _snakePattern: boolean; // reverse every second row of pixels in matrix
 
         /**
          * Shows all LEDs to a given color (range 0-255 for r, g, b).
@@ -191,14 +192,16 @@ namespace neopixel {
         /**
          * Sets the number of pixels in a matrix shaped strip
          * @param width number of pixels in a row
+         * @param snakePattern option if every second row is reversed
          */
-        //% blockId=neopixel_set_matrix_width block="%strip|set matrix width %width"
+        //% blockId=neopixel_set_matrix_width block="%strip|set matrix width %width|snake pattern %snakePattern"
         //% strip.defl=strip
         //% blockGap=8
         //% weight=5
         //% parts="neopixel" advanced=true
-        setMatrixWidth(width: number) {
+        setMatrixWidth(width: number, snakePattern: boolean) {
             this._matrixWidth = Math.min(this._length, width >> 0);
+            this._snakePattern = snakePattern;
         }
 
         /**
@@ -247,10 +250,33 @@ namespace neopixel {
         //% weight=79
         //% parts="neopixel"
         show() {
+            if (this._snakePattern) {
+                for (let y = 0; y < this._length / this._matrixWidth; y += 2) {
+                    for (let x = 0; x < this._matrixWidth / 2; x++) {
+                        const leftIndex = ((y * this._matrixWidth) + x) * 3; // Assuming RGB mode
+                        const rightIndex = ((y * this._matrixWidth) + (this._matrixWidth - 1 - x)) * 3;
+
+                        // Swap RGB values for the left and right pixels
+                        const tempR = this.buf[leftIndex];
+                        const tempG = this.buf[leftIndex + 1];
+                        const tempB = this.buf[leftIndex + 2];
+
+                        this.buf[leftIndex] = this.buf[rightIndex];
+                        this.buf[leftIndex + 1] = this.buf[rightIndex + 1];
+                        this.buf[leftIndex + 2] = this.buf[rightIndex + 2];
+
+                        this.buf[rightIndex] = tempR;
+                        this.buf[rightIndex + 1] = tempG;
+                        this.buf[rightIndex + 2] = tempB;
+                    }
+                }
+            }
+
             // only supported in beta
             // ws2812b.setBufferMode(this.pin, this._mode);
             ws2812b.sendBuffer(this.buf, this.pin);
         }
+        
 
         /**
          * Turn off all LEDs.
